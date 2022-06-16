97cf2b3600bc17abc586cba0d259aa273ea60f17
Gabriel Amorim <therealamorim@gmail.com>
2022-06-15 22:17:55 -0300

    feat: :tada: add initial structure

---
+++ src/main.ts
@@ -0,0 +1,8 @@
+import { AppModule } from './app.module';
+import { NestFactory } from '@nestjs/core';
+
+async function bootstrap(): Promise<void> {
+    const app = await NestFactory.create(AppModule);
+    await app.listen(3000);
+}
+bootstrap();
